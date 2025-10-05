5adfc677f2d6f731062a818485cddcb844b5ebd9
synono <synonolz@gmail.com>
2025-10-05 23:52:38 +0800

    优化.gitignore: 排除推送脚本

--- push.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "E:\MathExamVault\notebranch\mathnote\mathnote"
-Write-Host "Pushing to GitHub..."
-git add "错题集/"
-git commit -m "Update notes"
-git push origin main
-Write-Host "Done!"
-Read-Host "Press Enter to exit"
